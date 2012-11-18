0839e91cfce601a95d7789375108917d0039bec5
Amos Wenger <amos@movies.io>
2012-11-18 20:56:58 +0100

    Add notes about package managers

--- utils/apt-status.sh
+++ utils/apt-status.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+## apt is a front-end to dpkg, which installs .deb packages
+## on Debian-based distributions (Debian, Ubuntu, ...)
+##
+## apt-get is standard (as opposed to aptitude), and packages
+## can be installed via `sudo apt-get install PKG`.
+
 dpkg -s $1 &> /dev/null; INSTALLED=$?
 
 if [[ $INSTALLED -eq 0 ]]; then
--- utils/brew-status.sh
+++ utils/brew-status.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+## Brew is 'the missing package manager for OSX'
+## It is non-standard on OSX, but many devs have it because it
+## makes installing packages very easy.
+##
+## Installing packages is simply `brew install PKG` - since
+## it installs in /usr/local/Cellar (or an otherwise user-writeable
+## directory) there's no need to sudo.
+
 OUTPUT=$(brew info $1)
 STATUS=$?
 
